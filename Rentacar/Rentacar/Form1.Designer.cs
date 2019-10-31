c579ae1cb31eb4ebeb2ce0ce3a190bc2bc5bcd79
Iris Natashia <irisnatashia@gmail.com>
2019-10-31 00:11:25 -0400

    Initial

---
+++ Rentacar/Rentacar/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace Rentacar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.administracionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.gestionesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.empleadosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.rentasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.inspeccionesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tandasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.devolucionesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tandasToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.gestionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.vehiculosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.combustiblesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.administracionToolStripMenuItem,
+            this.gestionesToolStripMenuItem,
+            this.gestionToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // administracionToolStripMenuItem
+            // 
+            this.administracionToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.empleadosToolStripMenuItem,
+            this.tandasToolStripMenuItem,
+            this.tandasToolStripMenuItem1});
+            this.administracionToolStripMenuItem.Name = "administracionToolStripMenuItem";
+            this.administracionToolStripMenuItem.Size = new System.Drawing.Size(121, 24);
+            this.administracionToolStripMenuItem.Text = "Administracion";
+            // 
+            // gestionesToolStripMenuItem
+            // 
+            this.gestionesToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.rentasToolStripMenuItem,
+            this.inspeccionesToolStripMenuItem,
+            this.devolucionesToolStripMenuItem});
+            this.gestionesToolStripMenuItem.Name = "gestionesToolStripMenuItem";
+            this.gestionesToolStripMenuItem.Size = new System.Drawing.Size(79, 24);
+            this.gestionesToolStripMenuItem.Text = "Procesos";
+            // 
+            // empleadosToolStripMenuItem
+            // 
+            this.empleadosToolStripMenuItem.Name = "empleadosToolStripMenuItem";
+            this.empleadosToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
+            this.empleadosToolStripMenuItem.Text = "Empleados";
+            // 
+            // rentasToolStripMenuItem
+            // 
+            this.rentasToolStripMenuItem.Name = "rentasToolStripMenuItem";
+            this.rentasToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
+            this.rentasToolStripMenuItem.Text = "Rentas";
+            // 
+            // inspeccionesToolStripMenuItem
+            // 
+            this.inspeccionesToolStripMenuItem.Name = "inspeccionesToolStripMenuItem";
+            this.inspeccionesToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
+            this.inspeccionesToolStripMenuItem.Text = "Inspecciones";
+            // 
+            // tandasToolStripMenuItem
+            // 
+            this.tandasToolStripMenuItem.Name = "tandasToolStripMenuItem";
+            this.tandasToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
+            this.tandasToolStripMenuItem.Text = "Clientes";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // devolucionesToolStripMenuItem
+            // 
+            this.devolucionesToolStripMenuItem.Name = "devolucionesToolStripMenuItem";
+            this.devolucionesToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
+            this.devolucionesToolStripMenuItem.Text = "Devoluciones";
+            // 
+            // tandasToolStripMenuItem1
+            // 
+            this.tandasToolStripMenuItem1.Name = "tandasToolStripMenuItem1";
+            this.tandasToolStripMenuItem1.Size = new System.Drawing.Size(216, 26);
+            this.tandasToolStripMenuItem1.Text = "Tandas";
+            // 
+            // gestionToolStripMenuItem
+            // 
+            this.gestionToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.vehiculosToolStripMenuItem,
+            this.combustiblesToolStripMenuItem});
+            this.gestionToolStripMenuItem.Name = "gestionToolStripMenuItem";
+            this.gestionToolStripMenuItem.Size = new System.Drawing.Size(71, 24);
+            this.gestionToolStripMenuItem.Text = "Gestion";
+            // 
+            // vehiculosToolStripMenuItem
+            // 
+            this.vehiculosToolStripMenuItem.Name = "vehiculosToolStripMenuItem";
+            this.vehiculosToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
+            this.vehiculosToolStripMenuItem.Text = "Vehiculos";
+            // 
+            // combustiblesToolStripMenuItem
+            // 
+            this.combustiblesToolStripMenuItem.Name = "combustiblesToolStripMenuItem";
+            this.combustiblesToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
+            this.combustiblesToolStripMenuItem.Text = "Combustibles";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "FrmInicio";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem administracionToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem empleadosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem tandasToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem tandasToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem gestionesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem rentasToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem inspeccionesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem devolucionesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem gestionToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem vehiculosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem combustiblesToolStripMenuItem;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+    }
+}
+
---
+++ Rentacar/Rentacar/Form2.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace Rentacar
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CriterioCliente = new System.Windows.Forms.Label();
+            this.dgvClientes = new System.Windows.Forms.DataGridView();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.AddCliente = new System.Windows.Forms.Button();
+            this.txtCliente = new System.Windows.Forms.TextBox();
+            this.EditCliente = new System.Windows.Forms.Button();
+            this.DeleteCliente = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvClientes)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // CriterioCliente
+            // 
+            this.CriterioCliente.AutoSize = true;
+            this.CriterioCliente.Location = new System.Drawing.Point(32, 26);
+            this.CriterioCliente.Name = "CriterioCliente";
+            this.CriterioCliente.Size = new System.Drawing.Size(53, 17);
+            this.CriterioCliente.TabIndex = 0;
+            this.CriterioCliente.Text = "Criterio";
+            this.CriterioCliente.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // dgvClientes
+            // 
+            this.dgvClientes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvClientes.Location = new System.Drawing.Point(35, 78);
+            this.dgvClientes.Name = "dgvClientes";
+            this.dgvClientes.RowTemplate.Height = 24;
+            this.dgvClientes.Size = new System.Drawing.Size(480, 233);
+            this.dgvClientes.TabIndex = 1;
+            // 
+            // AddCliente
+            // 
+            this.AddCliente.Location = new System.Drawing.Point(320, 20);
+            this.AddCliente.Name = "AddCliente";
+            this.AddCliente.Size = new System.Drawing.Size(53, 23);
+            this.AddCliente.TabIndex = 2;
+            this.AddCliente.Text = "button1";
+            this.AddCliente.UseVisualStyleBackColor = true;
+            // 
+            // txtCliente
+            // 
+            this.txtCliente.Location = new System.Drawing.Point(103, 21);
+            this.txtCliente.Name = "txtCliente";
+            this.txtCliente.Size = new System.Drawing.Size(193, 22);
+            this.txtCliente.TabIndex = 5;
+            // 
+            // EditCliente
+            // 
+            this.EditCliente.Location = new System.Drawing.Point(379, 20);
+            this.EditCliente.Name = "EditCliente";
+            this.EditCliente.Size = new System.Drawing.Size(53, 23);
+            this.EditCliente.TabIndex = 6;
+            this.EditCliente.Text = "button2";
+            this.EditCliente.UseVisualStyleBackColor = true;
+            // 
+            // DeleteCliente
+            // 
+            this.DeleteCliente.Location = new System.Drawing.Point(438, 20);
+            this.DeleteCliente.Name = "DeleteCliente";
+            this.DeleteCliente.Size = new System.Drawing.Size(53, 23);
+            this.DeleteCliente.TabIndex = 7;
+            this.DeleteCliente.Text = "button3";
+            this.DeleteCliente.UseVisualStyleBackColor = true;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.DeleteCliente);
+            this.Controls.Add(this.EditCliente);
+            this.Controls.Add(this.txtCliente);
+            this.Controls.Add(this.AddCliente);
+            this.Controls.Add(this.dgvClientes);
+            this.Controls.Add(this.CriterioCliente);
+            this.Name = "Form2";
+            this.Text = "Cliente";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvClientes)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label CriterioCliente;
+        private System.Windows.Forms.DataGridView dgvClientes;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button AddCliente;
+        private System.Windows.Forms.TextBox txtCliente;
+        private System.Windows.Forms.Button EditCliente;
+        private System.Windows.Forms.Button DeleteCliente;
+    }
+}
